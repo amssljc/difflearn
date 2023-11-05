3495c6f287148595e80f584934cf2ee635c724d1
amssljc <amssljc@163.com>
2023-11-05 14:06:12 +0800

    描述你的提交

--- difflearn/visualization.py
+++ difflearn/visualization.py
@@ -2,16 +2,16 @@
 import seaborn as sns
 import numpy as np
 import torch 
+import matplotlib.pyplot as plt
 
-
-def show_matrix(matrix, ax=None, labels=None, title=None):
+def show_matrix(matrix, ax=None, labels='number', label_lenth = 11,  title=None):
     if isinstance(matrix, torch.Tensor):
         matrix = np.asarray(matrix)
     vmax = np.max(np.abs(matrix))
-
+    p  = matrix.shape[-1]
     cmap = sns.diverging_palette(260, 10, as_cmap=True)
-    if labels == None:
-        labels = range(matrix.shape[-1])
+    if labels == 'number':
+        labels  = list(np.linspace(0,p,label_lenth,dtype=int))
     if ax == None:
         ax = sns.heatmap(
             matrix,
@@ -19,8 +19,8 @@ def show_matrix(matrix, ax=None, labels=None, title=None):
             vmax=vmax,
             vmin=-vmax,
             square=True,
-            yticklabels=labels,
             xticklabels=labels,
+            yticklabels=labels,
         )
     else:
         sns.heatmap(
@@ -30,11 +30,17 @@ def show_matrix(matrix, ax=None, labels=None, title=None):
             vmin=-vmax,
             square=True,
             ax=ax,
-            yticklabels=labels,
             xticklabels=labels,
+            yticklabels=labels,
         )
 
-    if labels != None:
+        
+    if labels is not False:
+        xtick_positions = list(np.linspace(0,p,label_lenth,dtype=int))
+        ytick_positions = list(np.linspace(0,p,label_lenth,dtype=int))
+
+        ax.set_xticks(xtick_positions)
+        ax.set_yticks(ytick_positions)
         ax.set_xticklabels(labels, rotation=80, fontsize=5)
         ax.set_yticklabels(labels, rotation=10, fontsize=5)
     if title != None:
